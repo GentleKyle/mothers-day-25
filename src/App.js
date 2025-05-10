b8fb2e469d18e49877c7897fe9083594f4814771
GentleKyle <83362724+GentleKyle@users.noreply.github.com>
2025-05-10 16:23:54 -0600

    tailwindcss did not work - tried to fix - made a mess - tried to clean it up

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from './pages/home';
 function App() {
   return (
   <>
+  <div className='text-5xl'>HI</div>
     <Routes>
       <Route path='/' element={<Landing />} />
       <Route path='/home' element={<Home />} />
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,11 @@
 
 export default function home() {
     return (
-        <p>this is home</p>
+        <>
+            <div className="text-yellow-500">Nope</div>
+            <p className="bob">this is home</p>
+        </>
+        
+        
     )
 }
